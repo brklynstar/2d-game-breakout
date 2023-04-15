6a3889cb4ccaf4e64e0c9119c6070e8933d292cc
Brooke Finister <brookeafinister@gmail.com>
2023-04-15 09:34:29 -0700

    [UPDATE] created ball class

---
+++ ball.js
@@ -0,0 +1,10 @@
+class Ball {
+  constructor(x, y, dx, dy, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius;
+    this.color = color;
+  }
+}
