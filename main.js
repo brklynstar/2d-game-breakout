cb0fb5720feb8b0a9a231028a7a92629e915deff
Brooke Finister <brookeafinister@gmail.com>
2023-04-14 21:01:00 -0700

    [UPDATE] bouncing ball

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
+// Variables
+var ballRadius = 10;
+var x = canvas.width / 2;
+var y = canvas.height - 30;
+var dx = 2;
+var dy = -2;
 
-let x = canvas.width / 2;
-let y = canvas.height - 30;
-let dx = 2;
-let dy = -2;
 
-
+// Functions
 function drawball() {
-   
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI * 2);
-    ctx.fillStyle = "#0095DD";
+    ctx.arc(x, ballRadius, 10, 0, Math.PI * 2);
+    ctx.fillStyle = "red";
     ctx.fill();
     ctx.closePath();
 
@@ -21,12 +22,18 @@ function drawball() {
   function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawball();
+    
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
+    
     x += dx;
     y += dy;
   }
    
-
-
 
   setInterval(draw, 10);
   
