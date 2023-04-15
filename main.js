678bb561c102d7398f69e7757349d090a65652cf
Brooke Finister <brookeafinister@gmail.com>
2023-04-14 20:14:15 -0700

    [UPDATE] Cleaned up code in both files

--- main.js
+++ main.js
@@ -1,28 +1,32 @@
-const canvas = document.getElementById("myCanvas");
-const ctx = canvas.getContext("2d");
-const ballRadius =10;
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+
 let x = canvas.width / 2;
 let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
 
-function drawBall() {
-  ctx.beginPath();
-  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
-  ctx.fillStyle = "#0095DD";
-  ctx.fill();
-  ctx.closePath();
-}
 
-function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawBall();
-  x += dx;
-  y += dy;
-}
+function drawball() {
+   
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+
+  }
+
+  function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawball();
+    x += dx;
+    y += dy;
+  }
+   
 
 
-setInterval(draw, 10);
 
-
-
+  setInterval(draw, 10);
+  
