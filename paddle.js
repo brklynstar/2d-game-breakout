abcd0c4be3320fe21e3c2e04d9ebef79b7e77acd
Brooke Finister <brookeafinister@gmail.com>
2023-04-15 09:51:24 -0700

    [UPDATE]  brick and paddle class

---
+++ paddle.js
@@ -0,0 +1,17 @@
+class Paddle {
+  constructor(x, y, width, height, color) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+  }
+}
+
+// function drawPaddle() {
+//   ctx.beginPath();
+//   ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+//   ctx.fillStyle = 'black';
+//   ctx.fill();
+//   ctx.closePath();
+// }
