3ad9bff6524d92f84833a9db38afb6623323765c
Brooke Finister <brookeafinister@gmail.com>
2023-04-14 21:36:29 -0700

    [UPDATE] paddle and keyboard controls

--- main.js
+++ main.js
@@ -1,27 +1,56 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-
-// Variables
 var ballRadius = 10;
-var x = canvas.width / 2;
-var y = canvas.height - 30;
+var x = canvas.width/2;
+var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth)/2;
+var rightPressed = false;
+var leftPressed = false;
 
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
 
-// Functions
-function drawball() {
+function keyDownHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = true;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = false;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = false;
+    }
+}
+
+function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, ballRadius, 10, 0, Math.PI * 2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
     ctx.fillStyle = "red";
     ctx.fill();
     ctx.closePath();
+}
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "white";
+    ctx.fill();
+    ctx.closePath();
+}
 
-  }
-
-  function draw() {
+function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawball();
+    drawBall();
+    drawPaddle();
     
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
@@ -30,10 +59,21 @@ function drawball() {
         dy = -dy;
     }
     
+    if(rightPressed) {
+        paddleX += 7;
+        if (paddleX + paddleWidth > canvas.width){
+            paddleX = canvas.width - paddleWidth;
+        }
+    }
+    else if(leftPressed) {
+        paddleX -= 7;
+        if (paddleX < 0){
+            paddleX = 0;
+        }
+    }
+    
     x += dx;
     y += dy;
-  }
-   
+}
 
-  setInterval(draw, 10);
-  
+setInterval(draw, 10);
